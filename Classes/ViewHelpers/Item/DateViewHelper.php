3556138fb82baecfb0f842c4230bfa6d70354c42
Fredi Beglinger <f.beglinger@hispeed.ch>
2018-11-27 13:34:15 +0100

    Added Author and Date ViewHelper, New Template and edited formatting for Get_Date

---
+++ Classes/ViewHelpers/Item/DateViewHelper.php
@@ -0,0 +1,35 @@
+<?php
+namespace Fab\RssDisplay\ViewHelpers\Item;
+
+/*
+ * This file is part of the Fab/RssDisplay project under GPLv2 or later.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.md file that was distributed with this source code.
+ */
+
+use SimplePie_Item;
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * A View Helper which returns the "date" of a SimplePie item.
+ */
+class DateViewHelper extends AbstractViewHelper
+{
+
+    /**
+     * Retrieve the SimplePie item from the context and return its "date".
+     *
+     * @return string
+     * @throws \TYPO3\CMS\Fluid\Core\ViewHelper\Exception\InvalidVariableException
+     */
+    public function render()
+    {
+
+        /** @var SimplePie_Item $item */
+        $item = $this->templateVariableContainer->get('item');
+		$date_format = 'd.m.Y';
+        return $item->get_date ($date_format);
+    }
+
+}
